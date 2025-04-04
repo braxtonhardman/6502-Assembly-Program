d49e815d4329b172fc36f49f3682127c7ae01560
Ethan Emerson <eemerson0022@pointloma.edu>
2025-04-04 15:50:16 -0700

    Program #2 complete

--- Program2.asm
+++ Program2.asm
@@ -1,24 +1,38 @@
-jsr main 
-jsr helper 
+; Program 2: Ethan Emerson, Braxton Hardman, Christian Sapp
 
-main: 
-; Draw a pattern
-; $100 will keep track of the screen register
-LDA #$C8
-; $100 
-STA $A0
+; call the main subroutine
+JSR main
+BRK
 
-loop: 
-; Clear carry 
-CLC 
-; Increments register 100 by 4 
-LDA #$04 
-ADC $a0 
-STA $a0
-; Doesnt do anything really 
-CPY $FF
-BNE loop
 
-; helper changes the color of the drawing 
-jsr helper
-rts 
+; create the main subroutine
+main:
+
+    LDA #00 ; inital loop value
+    LDX #00 ; inital address value
+    LDY #00 ; initial inner loop value
+
+    ; main loop to change color
+    loop:
+        PHA ; push the A value to keep track of how many times we are looping
+        JSR changeColor
+        STA $0200,X
+        STA $0300,X
+        INX
+        STA $0200,X
+        STA $0300,X
+        INX
+        PLA
+        ADC #01
+        CLC
+        CMP #128 ; 16 colors before restarting so 8 rows of that = 128 End of row every 32 memory addresses (so that was the math worked out) 
+        BNE loop
+
+    RTS
+
+
+; color change subroutine just adds one to A
+changeColor:
+    ADC #$01
+    CLC
+    RTS
